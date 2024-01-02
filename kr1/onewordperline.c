bbd06912b6afffbc9906d86034f01aba479f3c6a
xanderlewis <me@xanderlewis.co.uk>
2024-01-02 17:44:12 +0000

    Clean up onewordperline

--- kr1/onewordperline.c
+++ kr1/onewordperline.c
@@ -1,5 +1,7 @@
 // Print the input one word per line.
 // ...so basically, convert each bit of whitespace into a newline character.
+// Whenever we have a non-blank, we just let it through. Most of the time we ignore whitespace.
+// The only thing we have to handle is when we first see whitespace after seeing a non-blank.
 
 #include <stdio.h>
 
@@ -10,20 +12,16 @@ int main() {
 	int state, c;
 
 	while((c = getchar()) != EOF) {
-		// If character is a blank (or some kind of whitespace character)
-		if (c == ' ' || c == '\t' || c == '\n') {
-			// If we're already in a word...
-			if (state == IN) {
-				state = OUT;
-				putchar('\n');
-			} else {
-				// If we're already outside of a word, do nothing.
-				;
-			}
+		// If character is a blank (or some kind of whitespace character) and we're in a word
+		if ((c == ' ' || c == '\t' || c == '\n') && state == IN) {
+			state = OUT;
+			putchar('\n');
 		} else {
 			// If character is a non-blank, let it through.
 			state = IN;
 			putchar(c);
 		}
 	}
+	putchar('\n');
+	return 0;
 }
